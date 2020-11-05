61f5893e09196b594e43491515cd2060f3ad86c8
Dmitry Sherbakov <dvsherbakov@gmail.com>
2020-11-05 18:21:21 +0500

    some changes

--- server/adress.model.ts
+++ server/adress.model.ts
@@ -1,1 +1,22 @@
 import { Document, Schema, Model, model } from 'mongoose'
+
+export interface IAdressInterface {
+  city: string;
+  street?: string;
+  home?: string;
+  additional?: string;
+}
+
+export interface IAdressModel extends Document, IAdressInterface {}
+
+const AdressSchema = new Schema({
+  city: { type: String, required: true },
+  street: String,
+  home: String,
+  additional: String,
+})
+
+export const Adress: Model<IAdressModel> = model<IAdressModel>(
+  'Adress',
+  AdressSchema
+)
--- server/organizaiton.models.ts
+++ server/organizaiton.models.ts
@@ -1,1 +1,7 @@
 import { Document, Schema, Model, model } from 'mongoose'
+import { IAdressModel, IAdressInterface } from './adress.model'
+
+export interface IOrganizationInterface {
+  name: string;
+  adress: IAdressInterface;
+}
