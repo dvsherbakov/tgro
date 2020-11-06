5b07a59bfe4d5d69e9e76aa245303bfdc7070b28
Dmitry Sherbakov <dvsherbakov@gmail.com>
2020-11-06 21:31:38 +0500

    some changes

--- server/organization.controller.ts
+++ server/organization.controller.ts
@@ -3,6 +3,24 @@ import { Organization } from './organizaiton.model'
 import { Adress, IAdressModel } from './adress.model'
 export const OrganizationRouter = express.Router()
 
+OrganizationRouter.get(
+  '/api/org',
+  async (
+    req: express.Request,
+    resp: express.Response,
+    next: express.NextFunction
+  ) => {
+    try {
+      const orgs = await Organization.find({})
+      if (orgs) {
+        resp.status(200).json(orgs)
+      } else resp.status(500).json({ message: 'Organization not found' })
+    } catch (err) {
+      resp.status(500).json({ message: `Internal server err: ${err}` })
+    }
+  }
+)
+
 OrganizationRouter.post(
   '/api/org',
   async (
@@ -14,7 +32,6 @@ OrganizationRouter.post(
       let adr: IAdressModel
       const { name, adress } = req.body
       let adrCandidate = await Adress.findOne(adress)
-      console.log(adrCandidate)
       if (adrCandidate) {
         adr = adrCandidate
       } else {
