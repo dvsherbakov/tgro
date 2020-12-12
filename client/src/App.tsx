cdba7a32ac8537e8d298af6f10ddcef4350836ed
Dmitry Sherbakov <dvsherbakov@gmail.com>
2020-12-12 14:07:57 +0500

    replase client on typescript redux template

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,21 @@
 import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
-import './App.css'
+import { Navbar } from './components/Navbar'
+import { About } from './pages/About'
+import { Home } from './pages/Home'
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <div className="container">
+        <Switch>
+          <Route path="/" component={Home} exact />
+          <Route path="/about" component={About} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   )
 }
 
