533a47159883fa7a42690157483dfb02b31e2c0b
Dmitry Sherbakov <dvsherbakov@gmail.com>
2020-12-23 22:04:27 +0500

    some changes

---
+++ client/src/Api/Api.ts
@@ -0,0 +1,23 @@
+import axios, { AxiosInstance, AxiosRequestConfig } from 'axios'
+
+export interface IApiOptions {
+  client?: AxiosInstance;
+  token?: string;
+  userId?: string;
+}
+
+export default class Api {
+  client: AxiosInstance
+  token: string
+
+  constructor(options: IApiOptions) {
+    const config: AxiosRequestConfig = {
+      withCredentials: true,
+      responseType: 'json',
+      baseURL: '/api/',
+    }
+
+    this.client = options.client || axios.create()
+    this.token = options.token || ''
+  }
+}
